19653afa70890fb44911c2f528ffedfb70b729a6
Rodney van den Velden <rodneymichael2002@gmail.com>
2023-02-11 20:00:17 +0100

    Command: fix serverpic

--- src/commands/util/serverpic.ts
+++ src/commands/util/serverpic.ts
@@ -1,8 +1,8 @@
 import { Command, CommandContext } from '../../../deps.ts';
 
 export default class ServerPictureCommand extends Command {
-  name = 'serverpicture';
-  aliases = [ 'serverpic', 'sp' ];
+  name = 'serverpic';
+  aliases = [ 'serverpicture', 'sp' ];
   category = 'util';
   description = 'Returns the server icon';
   usage = '';
