9939efc51ec46ae6f05188c3603be9e750169ada
Rodney van den Velden <rodneymichael2002@gmail.com>
2023-02-11 19:40:29 +0100

    commands: add help

---
+++ src/commands/util/help.ts
@@ -0,0 +1,23 @@
+import { Command, CommandContext, Embed } from '../../../deps.ts';
+
+export default class HelpCommand extends Command {
+  name = 'help';
+  aliases = [ 'h' ];
+  category = 'util';
+  description = 'Display this help message';
+  usage = '';
+  execute(ctx: CommandContext) {
+    const helpEmbed = new Embed()
+      .setFooter('Arguments: (optional) [required]', ctx.client.user!.avatarURL())
+      .setColor('GREEN')
+      .setTitle(`${ctx.client.user!.username} commands:`);
+    
+    for (const command of ctx.client.commands.list) {
+      helpEmbed.addField(`${ctx.client.prefix}${command[1].name} ${command[1].usage}`, command[1].description as string);
+    }
+
+    console.log('2');
+
+    ctx.channel.send(helpEmbed);
+  } 
+}
